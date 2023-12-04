12b6c9db5249ae5a8782486a2c1d2422d1063d46
Sergio Satyabrata <sergio.satyabrata@gmail.com>
2023-12-04 04:32:56 -0800

    fixing bugs

--- main.go
+++ main.go
@@ -1,23 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "github.com/nextbillion-ai/nb-optimization-sdk/src"
+	"fmt"
+
+	client "github.com/nextbillion-ai/nb-optimization-sdk/src"
 )
 
 func main() {
-    // Initialize the client
-    client := client.NewClient("http://localhost:8080") // Replace with the actual base URL of your server
+	client := client.NewClient("https://sgpstg.nextbillion.io/optimise-mvrp?key=plaintesting") // Replace with the actual base URL of your server
 
-    // Make a request (example: GET /optimise-mvrp/result)
-    resp, err := client.MakeRequest("/optimise-mvrp/result", "GET")
-    if err != nil {
-        fmt.Println("Error making request:", err)
-        return
-    }
-    defer resp.Body.Close()
+	// Make a request (example: GET /optimise-mvrp/result)
+	resp, err := client.MakeRequest("/optimise-mvrp/result", "GET")
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return
+	}
+	defer resp.Body.Close()
 
-    // Handle the response
-    fmt.Println("Response status:", resp.Status)
-    // Add more logic to handle the response body as needed
+	fmt.Println("Response status:", resp.Status)
 }
--- src/client.go
+++ src/client.go
@@ -17,15 +17,10 @@ func NewClient(baseURL string) *Client {
 }
 
 func (c *Client) MakeRequest(endpoint string, method string) (*http.Response, error) {
-    // Construct the full URL
     url := c.BaseURL + endpoint
-
-    // Create a new request
     req, err := http.NewRequest(method, url, nil)
     if err != nil {
         return nil, err
     }
-
-    // Send the request
     return c.httpClient.Do(req)
 }
