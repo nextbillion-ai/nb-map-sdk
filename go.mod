000073a804273fcd315859dfa56c54e446e22a7b
chang <chang@nextbillion.ai>
2023-12-05 09:42:27 +0800

    fix: compile and import errors and use AnyTypeLocation properly

--- go.mod
+++ go.mod
@@ -1,7 +1,17 @@
 module github.com/nextbillion-ai/nb-optimization-sdk
 
-go 1.21.4
+go 1.19
 
 // require github.com/nextbillion-ai/nb-optimization-interface v0.0.0-20231204152635-6da502567c4e
 
 require github.com/nextbillion-ai/nb-optimization-interface v0.0.0-20231204232635-6da502567c4e
+
+require (
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/nxadm/tail v1.4.11 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	// structs "github.com/nextbillion-ai/nb-optimization-interface"
@@ -10,56 +11,84 @@ import (
 	// validations "github.com/nextbillion-ai/nb-optimization-interface/validations"
 )
 
+func mkUint64Ptr(value uint64) *uint64 {
+	return &value
+}
 
 func main() {
-	apikey:=os.Getenv("APIKEY")
+	apikey := os.Getenv("APIKEY")
 	sdkClient := client.NewClient("https://api.nextbillion.io", apikey)
+	/*
+			{
+		  "locations": {
+		    "id": 50,
+		    "description": "Test",
+		    "location": "19.38972030,-99.14369731|19.42566371,-99.17081981|19.42436859,-99.13374095"
+		  },
+		  "vehicles": [{
+		    "id": 1,
+		    "start_index": 0,
+		    "end_index": 2
+		  }, {
+		    "id": 2,
+		    "start_index": 1,
+		    "end_index": 2
+		  }
+		  ],
+		  "jobs": [{
+		    "id": 1,
+		    "location_index": 0
+		  },{
+		    "id": 2,
+		    "location_index": 1
+		  }
+		  ]
+		}
+	*/
 	// Create an instance of OptimizationPostInput
 	input := structs.OptimizationPostInput{
 		Locations: structs.Locations{
-			Id:       50,
-			Location: "19.38972030,-99.14369731|19.42566371,-99.17081981|19.42436859,-99.13374095",
+			Id:              50,
+			AnyTypeLocation: "19.38972030,-99.14369731|19.42566371,-99.17081981|19.42436859,-99.13374095",
+			Location:        "19.38972030,-99.14369731|19.42566371,-99.17081981|19.42436859,-99.13374095",
 		},
 		Vehicles: []structs.Vehicle{
 			{
 				Id:         1,
-				StartIndex: 0,
-				EndIndex:   2,
-				Capacity:   []int64{4},
-				TimeWindow: []uint64{1656500400, 1656502860},
+				StartIndex: mkUint64Ptr(0),
+				EndIndex:   mkUint64Ptr(2),
 			},
 			{
 				Id:         2,
-				StartIndex: 1,
-				EndIndex:   2,
-				Capacity:   []int64{4},
-				TimeWindow: []uint64{1656500400, 1656502860},
+				StartIndex: mkUint64Ptr(1),
+				EndIndex:   mkUint64Ptr(2),
 			},
 		},
 		Jobs: []structs.Job{
 			{
 				Id:            1,
 				LocationIndex: 0,
-				Service:       300,
-				TimeWindows:   [][]uint64{{1656493200, 1656496800}},
 			},
 			{
 				Id:            2,
 				LocationIndex: 1,
-				Service:       300,
-				TimeWindows:   [][]uint64{{1656502200, 1656507600}},
 			},
 		},
 	}
 
 	// Make a POST request
-	resp, err := sdkClient.MakeRequest(fmt.Sprintf("/optimise-mvrp?key=%s",apikey),"POST", input)
+	resp, err := sdkClient.MakeRequest(fmt.Sprintf("/optimise-mvrp?key=%s", apikey), "POST", input)
+	if resp != nil {
+		defer resp.Body.Close()
+		body, re := io.ReadAll(resp.Body)
+		if re != nil {
+			fmt.Printf("Error getting response body:%s\n", re)
+		}
+		fmt.Printf("response body: %s \n", string(body))
+	}
 	if err != nil {
-		fmt.Println("Error making request:", err)
+		fmt.Printf("Error making request:%s\n", err)
 		return
 	}
-	defer resp.Body.Close()
-
 	fmt.Println("Response status:", resp.Status)
 }
-
